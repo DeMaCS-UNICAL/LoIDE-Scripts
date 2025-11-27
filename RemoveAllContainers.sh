687f25597eff4d4670548d59b93a699c4d7b6793
Marco Duca <dcumrc02a07d086r@studenti.unical.it>
2025-11-27 15:47:04 +0100

    Refined deploy and removal scripts

--- RemoveAllContainers.sh
+++ RemoveAllContainers.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-if [[ -n "$(docker ps -q)" ]]; then
-    ./StopPWA-API.sh
-    docker rm -vf $(docker ps -aq)
-else
-    echo "No containers to remove."
+targets=(api pwa ese)
+removed_any=false
+
+for name in "${targets[@]}"; do
+    if [[ -n "$(docker ps -a -q -f name=^/${name}$)" ]]; then
+        removed_any=true
+        if [[ -n "$(docker ps -q -f name=^/${name}$)" ]]; then
+            echo "Stopping $name container."
+            docker stop "$name"
+        fi
+        echo "Removing $name container."
+        docker rm -vf "$name"
+    fi
+done
+
+if [[ "$removed_any" = false ]]; then
+    echo "No api, pwa, or ese containers to remove."
 fi
